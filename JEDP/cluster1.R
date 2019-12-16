b40a4febaca099a45a7aecc88e9713c08fca09f9
Daniel Dorado <37589053+apocop@users.noreply.github.com>
2019-12-16 09:25:00 -0800

    [JEDP] Create new Tanakh dataset.

--- JEDP/cluster1.R
+++ JEDP/cluster1.R
@@ -8,3 +8,11 @@ book_names <- read_tsv("asv/book_names.txt", col_names = c("orig_book_index", "n
 
 # Add "name" column to bible.
 bible <- plyr::join(bible, book_names, by = "orig_book_index")
+
+# Create a new column: O/N for Old/New Testament 
+bible$canon <- unlist(str_extract_all(bible$orig_book_index, "[[:alpha:]]"))
+
+# Subset the Hebrew Bible from the Christian Bible.
+tanakh <- subset(bible, canon == "O")
+
+view(tanakh)
