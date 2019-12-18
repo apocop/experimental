67ba147dd62c5e75c691a779a41f5fee325b7683
Daniel Dorado <37589053+apocop@users.noreply.github.com>
2019-12-18 10:50:12 -0800

    [tokenizer] Code cleanup.

--- tokenizer/tokenizer.py
+++ tokenizer/tokenizer.py
@@ -1,5 +1,9 @@
-class Token(object):
+"""
+A tokenizer example for English text.
+"""
 
+class Token(object):
+    """A token object."""
     def __init__(self, token):
         self.token = token
 
@@ -8,12 +12,16 @@ class Token(object):
 
 
 class Tokenizer(object):
+    "Tokenizes a string and returns a Document of Tokens."
 
     def __init__(self):
 
         self.tokens = []
-        self.inital_punctuation = ['\'', '"']
-        self.final_punctuation = ['\'', '"', '!', '?', '!', '/', '\\', '.', ',']
+        self.token_cache = []
+
+        # English specific customizations.
+        self.inital_punctuation = ('\'', '"')
+        self.final_punctuation = ('\'', '"', '!', '?', '!', '/', '\\', '.', ',')
         self.token_exceptions = {
             'don\'t' : ('do', 'n\'t'),
             'doen\'t': ('does', 'n\'t'),
@@ -22,12 +30,12 @@ class Tokenizer(object):
             ':(': (':(',),
         }
 
-        self.token_cache = []
-
     def add_token(self, approved_token):
+        """Add an approved token to the final token list."""
         self.tokens.append(Token(approved_token))
 
     def tokenize(self, text):
+        """Go through the tokenization pipeline to produce tokens."""
 
         for preprocessed_token in text.split():
 
@@ -41,8 +49,6 @@ class Tokenizer(object):
             if preprocessed_token:
                 self.add_token(preprocessed_token)
 
-
-
         return Document(self.tokens)
 
 
@@ -53,6 +59,7 @@ class Tokenizer(object):
 
 
     def split_prefix(self, preprocessed_token):
+        "Split initial punctuation."
         if preprocessed_token[0] in self.inital_punctuation:
             self.add_token(preprocessed_token[0])
             preprocessed_token = self.split_prefix(preprocessed_token[1:])
@@ -61,62 +68,46 @@ class Tokenizer(object):
             return preprocessed_token
 
     def split_suffix(self, preprocessed_token):
+        "Stem final punctuation."
 
         suffix = preprocessed_token[-1]
-
         if suffix in self.final_punctuation:
-
             split_token = preprocessed_token[:-1]
-
             self.add_to_token_cache(suffix)
-            #self.split_suffix(preprocessed_token[:-1])
-            print(f'preprocessed token: {preprocessed_token[:-1]}')
 
             if split_token[-1] in self.final_punctuation:
                 self.split_suffix(split_token)
             else:
                 self.add_to_token_cache(split_token)
 
-
             for i in range(0, len(self.token_cache)):
-
                 token = self.pop_from_token_cache()
-
                 if preprocessed_token in self.token_exceptions:
                     self.add_exceptions(preprocessed_token)
                     continue
-            
                 self.add_token(token)
-
-            # self.clear_token_cache()
-
             return None
-
-
         else:
             return preprocessed_token
 
     def add_to_token_cache(self, token):
         self.token_cache.append(token)
-        print(self.token_cache)
 
     def pop_from_token_cache(self):
         return self.token_cache.pop()
 
-    def clear_token_cache(self):
-        self.token_cache = []
 
 class Document(object):
+    """Container Object for Tokens."""
  
     def __init__(self, tokens):
-        self.tokens = tokens
-        # self.tokens = set(tokens)
+        self.tokens = tuple(tokens)
 
     def __len__(self):
         return len(self.tokens)
 
 
-
+# Temporary Tests.
 s = "Apple is looking at buying U.K. startup for $ 1billion."
 s = '"Apple'
 s = "'I don't have an apple.' Apple is looking at buying U.K. startup for $1 billion."
@@ -124,13 +115,18 @@ s = "'I don't have an apple.'"
 s = "'I don't have an apple,' he said."
 
 s = """
-These aren’t trivial numbers. Compared to placebo, 20 more people were dying every year when taking these two supplements. Over the four years of the trial, that equates to 80 more deaths. As the authors wrote at the time, “The present findings provide ample grounds to discourage use of supplemental beta-carotene and the combination of beta-carotene and vitamin A.”
+These aren’t trivial numbers.
+Compared to placebo, 20 more people were dying every year when taking these
+two supplements. Over the four years of the trial, that equates to 80 more
+deaths. As the authors wrote at the time, “The present findings provide
+ample grounds to discourage use of supplemental beta-carotene and the
+combination of beta-carotene and vitamin A.”
 """
 s = 'I love you. :) Not!!! :('
+
 tokenizer = Tokenizer()
 processed_text = tokenizer.tokenize(s)
 
-
-print(len(processed_text))
-for x in range(0, len(processed_text.tokens)):
-    print(processed_text.tokens[x])
+print(f'Number of tokens: {len(processed_text)}')
+for token in processed_text.tokens:
+    print(token)
