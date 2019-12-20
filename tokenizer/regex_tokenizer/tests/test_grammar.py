9ce5b512aff9e22e9cc4917b54e38160468dc460
Daniel Dorado <37589053+apocop@users.noreply.github.com>
2019-12-20 10:30:50 -0800

    [tokenizer] Update tests.

--- tokenizer/regex_tokenizer/tests/test_grammar.py
+++ tokenizer/regex_tokenizer/tests/test_grammar.py
@@ -2,8 +2,7 @@ from locales import en
 
 def test_regex_rules():
     tokenizer_rules = en.tokenizer_rules
-    assert tokenizer_rules['INITIAL_PUNCTUATION_TOKEN'] == """^(['"])([A-Z]+)$"""
-    assert tokenizer_rules['FINAL_PUNCTUATION_TOKEN'] == """^([A-Z]+)([',!?":.]+)$"""
-    assert tokenizer_rules['ALL_PUNCTUATION_TOKEN'] == """^([',!?":.])([',!?":.]+)$"""
-    assert tokenizer_rules['CURRENCY_AMOUNT_TOKEN'] == """^([$£¥])([0-9]+\.?[0-9]{,2})$"""
-
+    assert tokenizer_rules['INITIAL_PUNCTUATION_TOKEN'] == r"""^(['"])([A-Z]+)$"""
+    assert tokenizer_rules['FINAL_PUNCTUATION_TOKEN'] == r"""^([A-Z]+)([',!?":.]+)$"""
+    assert tokenizer_rules['ALL_PUNCTUATION_TOKEN'] == r"""^([',!?":.])([',!?":.]+)$"""
+    assert tokenizer_rules['CURRENCY_AMOUNT_TOKEN'] == r"""^([$£¥])([0-9]+\.?[0-9]{,2})$"""
