921cd6ba4e4b07e76a4a7f59c2eb25dca7c5529a
Daniel Dorado <37589053+apocop@users.noreply.github.com>
2019-12-19 23:01:04 -0800

    Complete basic regex tokenizer.

--- tokenizer/regex_tokenizer.py
+++ tokenizer/regex_tokenizer.py
@@ -1,13 +1,15 @@
 import re
 
-sample =  """"I don't like that dog", he remarked.""" 
+sample =  """"I don't like that dog!!!", he remarked.""" 
 
 alpha = '[A-Z]'
 inital_punctuation = '[\'"]'
-final_punctuation = '[]'
+final_punctuation = '[\',!?"]'
 
 conditions = {
-    'inital_punctuation' : f'({inital_punctuation})({alpha}*)'
+    'inital_punctuation' : f'({inital_punctuation})({alpha}+)',
+    'final_punctuation' : f'({alpha}+)({final_punctuation}+)',
+    'all_punctuation' : f'({final_punctuation})({final_punctuation}+)',
 }
 
 
@@ -27,21 +29,31 @@ def tokenize(text):
                 tokenize(token)
 
         else:
+            # Create Conditions.
+            initial_punctuation = re.match(conditions.get('inital_punctuation'), span, re.IGNORECASE)
+            final_punctuation = re.match(conditions.get('final_punctuation'), span, re.IGNORECASE)
+            all_punctuation = re.match(conditions.get('all_punctuation'), span, re.IGNORECASE)
+
             # Intial Punctuation.
-            has_initial_punctuation = re.match(conditions.get('inital_punctuation'), span, re.IGNORECASE)
-            if has_initial_punctuation:
-                print(has_initial_punctuation.group(0))
-            # Final Puctuation
-            elif re.match('{alpha}{final_punctuation}*', span, re.IGNORECASE):
-                pass
+            if initial_punctuation:
+                g1, g2 = initial_punctuation.group(1), initial_punctuation.group(2)
+                tokenize(g1)
+                tokenize(g2)
+            # Final Punctuation.
+            elif final_punctuation:
+                g1, g2 = final_punctuation.group(1), final_punctuation.group(2)
+                tokenize(g1)
+                tokenize(g2)
+            # All Punctuation.
+            elif all_punctuation:
+                g1, g2 = all_punctuation.group(1), all_punctuation.group(2)
+                tokenize(g1)
+                tokenize(g2)
             else:
                 tokens.append(span)
-
-    
-
     return tokens
 
 
 tokens = tokenize(sample)
 for token in tokens:
-    print(token)
+    print("Token", token)
