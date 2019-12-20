003cd2974b13b12c33480c53794901b54151da8b
Daniel Dorado <37589053+apocop@users.noreply.github.com>
2019-12-20 14:37:32 -0800

    [tokenizer] Update inital punctuation rule to allow for final punctuation in group 2.

--- tokenizer/regex_tokenizer/tests/test_grammar.py
+++ tokenizer/regex_tokenizer/tests/test_grammar.py
@@ -1,8 +1,15 @@
 from locales import en
 
-def test_regex_rules():
-    tokenizer_rules = en.tokenizer_rules
-    assert tokenizer_rules['INITIAL_PUNCTUATION_TOKEN'] == r"""^(['"])([A-Z]+)$"""
-    assert tokenizer_rules['FINAL_PUNCTUATION_TOKEN'] == r"""^([A-Z]+)([',!?":.]+)$"""
-    assert tokenizer_rules['ALL_PUNCTUATION_TOKEN'] == r"""^([',!?":.])([',!?":.]+)$"""
-    assert tokenizer_rules['CURRENCY_AMOUNT_TOKEN'] == r"""^([$£¥])([0-9]+\.?[0-9]{,2})$"""
+tokenizer_rules = en.rules
+
+def test_inital_punctuation_rule():
+    assert tokenizer_rules['INITIAL_PUNCTUATION_TOKEN'].pattern == """^([\'"])([A-Z]+[\',!?":.]*)$"""
+
+def test_final_punctuation_rule():
+    assert tokenizer_rules['FINAL_PUNCTUATION_TOKEN'].pattern == """^([A-Z]+)([',!?":.]+)$"""
+
+def test_all_punctuation_rule():
+    assert tokenizer_rules['ALL_PUNCTUATION_TOKEN'].pattern == """^([',!?":.])([',!?":.]+)$"""
+
+def test__punctuation_rule():
+    assert tokenizer_rules['CURRENCY_AMOUNT_TOKEN'].pattern == r"""^([$£¥])([0-9]+\.?[0-9]{,2})$"""
