021de2e9678cdfb94a89ef7a6418dea8890a9afe
Daniel Dorado <37589053+apocop@users.noreply.github.com>
2019-12-19 23:55:14 -0800

    [tokenizer] Add support for currency.

--- tokenizer/regex_tokenizer.py
+++ tokenizer/regex_tokenizer.py
@@ -1,54 +1,74 @@
 import re
 
-sample =  """"I don't like that dog!!!", he remarked.""" 
+
+
+sample =  """"I don't like that dog", he remarked.""" 
+sample = "Apple is looking at buying U.K. startup for $1 billion."
+
+# sample = """
+# These aren’t trivial numbers.
+# Compared to placebo, 20 more people were dying every year when taking these
+# two supplements. Over the four years of the trial, that equates to 80 more
+# deaths. As the authors wrote at the time, “The present findings provide
+# ample grounds to discourage use of supplemental beta-carotene and the
+# combination of beta-carotene and vitamin A.”
+# """
+# sample = 'I love you. :) Not!!! :('
+
+# sample = "U.K."
+
 
 alpha = '[A-Z]'
+digits = '[0-9]'
 inital_punctuation = '[\'"]'
-final_punctuation = '[\',!?"]'
+final_punctuation = '[\',!?":.]'
+currency_symbol = '[$£¥]'
+boundary = '\B'
+
 
 conditions = {
-    'inital_punctuation' : f'({inital_punctuation})({alpha}+)',
-    'final_punctuation' : f'({alpha}+)({final_punctuation}+)',
-    'all_punctuation' : f'({final_punctuation})({final_punctuation}+)',
+    'inital_punctuation' : f'{boundary}({inital_punctuation})({alpha}+){boundary}',
+    'final_punctuation' : f'{boundary}({alpha}+)({final_punctuation}+){boundary}',
+    'all_punctuation' : f'{boundary}({final_punctuation})({final_punctuation}+){boundary}',
+    'currency_amount' : f'({currency_symbol})({digits}+\.?{digits}*)',
 }
+
+exceptions = {
+    "don't" : ['do', 'not']
+    }
 
 
 tokens = []
 
 def tokenize(text):
-
-    exceptions = {
-    "don't" : ['do', 'not']
-    }
-
-    for span in text.split(' '):
-
+    for span in text.split():
         # Exceptions.
         if span in exceptions:
             for token in exceptions.get(span):
                 tokenize(token)
-
         else:
             # Create Conditions.
             initial_punctuation = re.match(conditions.get('inital_punctuation'), span, re.IGNORECASE)
             final_punctuation = re.match(conditions.get('final_punctuation'), span, re.IGNORECASE)
             all_punctuation = re.match(conditions.get('all_punctuation'), span, re.IGNORECASE)
+            currency_amount = re.match(conditions.get('currency_amount'), span, re.IGNORECASE)
 
             # Intial Punctuation.
             if initial_punctuation:
-                g1, g2 = initial_punctuation.group(1), initial_punctuation.group(2)
-                tokenize(g1)
-                tokenize(g2)
+                tokenize(initial_punctuation.group(1))
+                tokenize(initial_punctuation.group(2))
             # Final Punctuation.
             elif final_punctuation:
-                g1, g2 = final_punctuation.group(1), final_punctuation.group(2)
-                tokenize(g1)
-                tokenize(g2)
+                tokenize(final_punctuation.group(1))
+                tokenize(final_punctuation.group(2))
             # All Punctuation.
             elif all_punctuation:
-                g1, g2 = all_punctuation.group(1), all_punctuation.group(2)
-                tokenize(g1)
-                tokenize(g2)
+                tokenize(all_punctuation.group(1))
+                tokenize(all_punctuation.group(2))
+            # Currency amount.
+            elif currency_amount:
+                tokenize(currency_amount.group(1))
+                tokenize(currency_amount.group(2))
             else:
                 tokens.append(span)
     return tokens
